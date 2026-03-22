952efd25223e3f11cd6a667341dfe32d6775df56
Joe R. Quitalig <billyjoequitalig@gmail.com>
2026-03-23 03:18:44 +0800

    rename

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyPorfolio.Pages
+namespace MyPortfolio.Pages
 {
     public class ContactModel : PageModel
     {
